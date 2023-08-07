9abb57e5be01ccf6a2b4defe3b91072b5fd99f17
Christopher Battista <cattista@gmail.com>
2023-08-06 21:57:52 -0400

    Prettied up Latest Podcasts

---
+++ script/youtubeModal.js
@@ -0,0 +1,9 @@
+function openYouTubeModal() {
+    var modal = document.getElementById("youtubeModal");
+    modal.style.display = "block";
+}
+
+function closeYouTubeModal() {
+    var modal = document.getElementById("youtubeModal");
+    modal.style.display = "none";
+}
