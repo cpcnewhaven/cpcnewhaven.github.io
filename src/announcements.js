6d48d97af50af01ce61dae0466b36c6b70f7198c
cpcnewhaven <avteam@cpcnewhaven.org>
2023-12-17 11:00:14 -0500

    created an announcements page - created announcements javascript - have it pull from the json file announcements from the week itself

---
+++ src/announcements.js
@@ -0,0 +1,34 @@
+// Function to create announcement HTML
+function createAnnouncementHtml(announcement) {
+    var title = announcement.title || 'No Title';
+    var date = announcement.date || '';
+    var time = announcement.time || '';
+    var description = announcement.description || '';
+    var schedule = announcement.schedule || '';
+
+    return `
+        <div class="announcement">
+            <h2>${title}</h2>
+            ${date ? `<p><strong>Date:</strong> ${date}</p>` : ''}
+            ${time ? `<p><strong>Time:</strong> ${time}</p>` : ''}
+            ${schedule ? `<p><strong>Schedule:</strong> ${schedule}</p>` : ''}
+            <p>${description}</p>
+        </div>
+    `;
+}
+
+// Function to render announcements
+function renderAnnouncements(announcements) {
+    var announcementsContainer = document.getElementById('announcements');
+    var htmlContent = announcements.map(createAnnouncementHtml).join('');
+    announcementsContainer.innerHTML = htmlContent;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('../data/announcements/announcements_week50-23.json')
+        .then(response => response.json())
+        .then(data => {
+            renderAnnouncements(data.announcements);
+        })
+        .catch(error => console.error('Error fetching data:', error));
+});
