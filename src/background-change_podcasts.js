2428976b59364d774867b6dc9eac5c743b31db68
cpcnewhaven <avteam@cpcnewhaven.org>
2023-12-31 11:10:12 -0500

    added bg and javascript for podcasts

---
+++ src/background-change_podcasts.js
@@ -0,0 +1,20 @@
+// JavaScript code to randomly change background images every 5 seconds
+
+function changeBackgroundImage() {
+    var min = 2;
+    var max = 4;
+    var randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+    var imageUrl = '/assets/websiteBG-podcasts/' + randomNum + '.png';
+    var worshipElement = document.getElementById('podcasts');
+    worshipElement.style.opacity = 0; // Start with the image invisible
+    worshipElement.style.backgroundImage = 'url(' + imageUrl + ')';
+    setTimeout(function() {
+      worshipElement.style.opacity = 1; // Fade in the new image
+    }, 100); // Adjust the duration as needed
+  }
+
+// Change background image on page load
+changeBackgroundImage();
+
+// Change background image every 5 seconds
+setInterval(changeBackgroundImage, 7500);
