bfd84c31560fa22a3d8787601eb174582a528e27
cpcnewhaven <avteam@cpcnewhaven.org>
2024-06-23 10:52:40 -0400

    added to the links and layout

--- src/js/updateSermonContent.js
+++ src/js/updateSermonContent.js
@@ -2,15 +2,20 @@ document.addEventListener('DOMContentLoaded', function() {
   fetch('./data/upcomingSermon.json')
     .then(response => response.json())
     .then(data => {
-      const sermonTitle = document.getElementById('sermonTitle');
-      const sermonScripture = document.getElementById('sermonScripture');
-      const sermonDate = document.getElementById('sermonDate');
-      const pastorName = document.getElementById('pastorName');
+      const activeSermon = data.find(sermon => sermon.active);
+      if (activeSermon) {
+        const sermonTitle = document.getElementById('sermonTitle');
+        const sermonScripture = document.getElementById('sermonScripture');
+        const sermonDate = document.getElementById('sermonDate');
+        const pastorName = document.getElementById('pastorName');
 
-      sermonTitle.textContent = data.title;
-      sermonScripture.textContent = data.scripture;
-      sermonDate.textContent = `Date: ${data.date}`;
-      pastorName.textContent = data.author;
+        sermonTitle.textContent = activeSermon.title;
+        sermonScripture.textContent = activeSermon.scripture;
+        sermonDate.textContent = `Date: ${activeSermon.date}`;
+        pastorName.textContent = activeSermon.author;
+      } else {
+        console.error('No active sermon found.');
+      }
     })
     .catch(error => console.error('Error loading the sermon data:', error));
 });
