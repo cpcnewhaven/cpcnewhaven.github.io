b24f185f232716ba249aae6373451a2d8638d91b
cpcnewhaven <avteam@cpcnewhaven.org>
2024-07-21 11:34:47 -0400

    updated the index worship widget and made it a little bit more sexy - also made it work with the graphic

--- src/js/updateSermonContent.js
+++ src/js/updateSermonContent.js
@@ -8,11 +8,15 @@ document.addEventListener('DOMContentLoaded', function() {
         const sermonScripture = document.getElementById('sermonScripture');
         const sermonDate = document.getElementById('sermonDate');
         const pastorName = document.getElementById('pastorName');
+        const sermonYoutube = document.getElementById('sermonYoutube');
+        const sermonBulletin = document.getElementById('sermonBulletin');
 
         sermonTitle.textContent = activeSermon.title;
         sermonScripture.textContent = activeSermon.scripture;
         sermonDate.textContent = `Date: ${activeSermon.date}`;
         pastorName.textContent = activeSermon.author;
+        sermonYoutube.href = activeSermon.youtube_url;
+        sermonBulletin.href = activeSermon.bulletin_url;
       } else {
         console.error('No active sermon found.');
       }
