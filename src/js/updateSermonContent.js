5dddc9bf5af9c04ec602c50cb52d9ec75008bc22
agSuperCoder123 <alex@ahoy.ooo>
2024-08-12 16:16:10 -0400

    added some content

--- src/js/updateSermonContent.js
+++ src/js/updateSermonContent.js
@@ -10,13 +10,41 @@ document.addEventListener('DOMContentLoaded', function() {
         const pastorName = document.getElementById('pastorName');
         const sermonYoutube = document.getElementById('sermonYoutube');
         const sermonBulletin = document.getElementById('sermonBulletin');
+        const sermonSpotify = document.getElementById('sermonSpotify');
+        const sermonApplePodcasts = document.getElementById('sermonApplePodcasts');
 
         sermonTitle.textContent = activeSermon.title;
         sermonScripture.textContent = activeSermon.scripture;
         sermonDate.textContent = `Date: ${activeSermon.date}`;
         pastorName.textContent = activeSermon.author;
-        sermonYoutube.href = activeSermon.youtube_url;
-        sermonBulletin.href = activeSermon.bulletin_url;
+        
+        if (activeSermon.youtube_url) {
+          sermonYoutube.href = activeSermon.youtube_url;
+          sermonYoutube.style.display = 'block';
+        } else {
+          sermonYoutube.style.display = 'none';
+        }
+        
+        if (activeSermon.bulletin_url) {
+          sermonBulletin.href = activeSermon.bulletin_url;
+          sermonBulletin.style.display = 'block';
+        } else {
+          sermonBulletin.style.display = 'none';
+        }
+        
+        if (activeSermon.spotify_url) {
+          sermonSpotify.href = activeSermon.spotify_url;
+          sermonSpotify.style.display = 'block';
+        } else {
+          sermonSpotify.style.display = 'none';
+        }
+        
+        if (activeSermon.apple_podcasts_url) {
+          sermonApplePodcasts.href = activeSermon.apple_podcasts_url;
+          sermonApplePodcasts.style.display = 'block';
+        } else {
+          sermonApplePodcasts.style.display = 'none';
+        }
       } else {
         console.error('No active sermon found.');
       }
