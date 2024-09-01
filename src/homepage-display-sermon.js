82791c2c28f1628fc079ac37b2ff19f281b1df86
cpcnewhaven <avteam@cpcnewhaven.org>
2024-09-01 11:06:18 -0400

    made it dynamic

--- src/homepage-display-sermon.js
+++ src/homepage-display-sermon.js
@@ -10,6 +10,7 @@
           document.getElementById('sermonBulletin').href = activeSermon.bulletin_url || '#';
           document.querySelector('.ag-worship-image').src = activeSermon['podcast-thumbnail_url'] || 'default-image.jpg';
           document.querySelector('.ag-worship-image').alt = activeSermon.title || 'Sermon Image';
+          document.getElementById('pastorName').textContent = activeSermon.author || 'Unknown Pastor';
         }
       })
       .catch(error => console.error('Error fetching sermon data:', error));
