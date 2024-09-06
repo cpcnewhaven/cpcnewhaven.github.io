ec40ce26e662c52a87bac7820da1bf4fb4525cbf
agSuperCoder123 <alex@ahoy.ooo>
2024-09-06 12:21:16 -0400

    update highlights and what not for alexis

--- src/js/populate-ongoing-events.js
+++ src/js/populate-ongoing-events.js
@@ -5,18 +5,19 @@ document.addEventListener('DOMContentLoaded', function() {
             const eventsTable = document.getElementById('ongoingEventsTable');
             const tbody = eventsTable.getElementsByTagName('tbody')[0];
 
-            // Sort events by date (assuming event.date is in a valid date format)
-            data.ongoingEvents.sort((a, b) => new Date(a.date) - new Date(b.date));
+            // Filter out inactive events
+            const activeEvents = data.ongoingEvents.filter(event => event.active === "true");
 
-            data.ongoingEvents.forEach(event => {
+            // Sort events by date
+            activeEvents.sort((a, b) => new Date(a.dateEntered) - new Date(b.dateEntered));
+
+            activeEvents.forEach(event => {
                 const row = tbody.insertRow();
                 const titleCell = row.insertCell(0);
                 const descriptionCell = row.insertCell(1);
-                // Removed dateCell and its usage
 
                 titleCell.textContent = event.title;
                 descriptionCell.innerHTML = event.description; // Use innerHTML to render HTML content
-                // Removed dateCell.textContent assignment
             });
         })
         .catch(error => console.error('Error loading the events:', error));
