14d064d884b63e6e3272458ec594a5ab8fcb0ddd
agBabbbyCakes <108819253+agBabbbyCakes@users.noreply.github.com>
2024-11-19 14:51:22 -0500

    updated the index links with the correct sermon links

--- src/homepage-display-sermon.js
+++ src/homepage-display-sermon.js
@@ -1,13 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-  fetch('data/upcomingSermon.json')
+  fetch('data/podcast-index.json')
     .then(response => response.json())
     .then(data => {
-      const activeSermon = data.find(sermon => sermon.active && sermon.status === 'present');
-      if (activeSermon) {
-        document.getElementById('sermonTitle').textContent = activeSermon.title || 'Sermon Title';
-        document.getElementById('sermonScripture').textContent = activeSermon.scripture || 'Scripture';
-        document.querySelector('.ag-worship-image').src = activeSermon['podcast-thumbnail_url'] || 'default-image.jpg';
-        document.querySelector('.ag-worship-image').alt = activeSermon.title || 'Sermon Image';
+      const sundaySermons = data.sundaySermons.episodes;
+      const latestSermon = sundaySermons.reduce((latest, sermon) => {
+        return new Date(sermon.date) > new Date(latest.date) ? sermon : latest;
+      }, sundaySermons[0]);
+
+      if (latestSermon) {
+        document.getElementById('sermonTitle').textContent = latestSermon.title || 'Sermon Title';
+        document.getElementById('sermonScripture').textContent = latestSermon.scripture || 'Scripture';
+        document.querySelector('.ag-worship-image').src = latestSermon['podcast-thumbnail_url'] || 'default-image.jpg';
+        document.querySelector('.ag-worship-image').alt = latestSermon.title || 'Sermon Image';
       }
     })
     .catch(error => console.error('Error fetching sermon data:', error));
