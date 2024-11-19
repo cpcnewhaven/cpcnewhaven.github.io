3438e53a7f526cea2ce1abdca1fd387d6d42cea2
agBabbbyCakes <108819253+agBabbbyCakes@users.noreply.github.com>
2024-11-18 23:01:52 -0500

    hotfix update on homepage sermon

--- src/homepage-display-sermon.js
+++ src/homepage-display-sermon.js
@@ -1,19 +1,14 @@
-  document.addEventListener('DOMContentLoaded', function() {
-    fetch('data/upcomingSermon.json')
-      .then(response => response.json())
-      .then(data => {
-        const activeSermon = data.find(sermon => sermon.active && sermon.status === 'present');
-        if (activeSermon) {
-          document.getElementById('sermonSpotify').href = activeSermon.spotify_url || '#';
-          document.getElementById('sermonApple').href = activeSermon.apple_podcasts_url || '#';
-          document.getElementById('sermonYoutube').href = activeSermon.youtube_url || '#';
-          document.getElementById('sermonBulletin').href = activeSermon.bulletin_url || '#';
-          document.querySelector('.ag-worship-image').src = activeSermon['podcast-thumbnail_url'] || 'default-image.jpg';
-          document.querySelector('.ag-worship-image').alt = activeSermon.title || 'Sermon Image';
-          document.getElementById('sermonTitle').textContent = activeSermon.title || 'Sermon Title'; // Update sermon title
-          document.getElementById('sermonScripture').textContent = activeSermon.scripture || 'Scripture'; // Update sermon scripture
-          document.getElementById('pastorName').textContent = activeSermon.author || 'Unknown Pastor';
-        }
-      })
-      .catch(error => console.error('Error fetching sermon data:', error));
-  });
+document.addEventListener('DOMContentLoaded', function() {
+  fetch('data/upcomingSermon.json')
+    .then(response => response.json())
+    .then(data => {
+      const activeSermon = data.find(sermon => sermon.active && sermon.status === 'present');
+      if (activeSermon) {
+        document.getElementById('sermonTitle').textContent = activeSermon.title || 'Sermon Title';
+        document.getElementById('sermonScripture').textContent = activeSermon.scripture || 'Scripture';
+        document.querySelector('.ag-worship-image').src = activeSermon['podcast-thumbnail_url'] || 'default-image.jpg';
+        document.querySelector('.ag-worship-image').alt = activeSermon.title || 'Sermon Image';
+      }
+    })
+    .catch(error => console.error('Error fetching sermon data:', error));
+});
