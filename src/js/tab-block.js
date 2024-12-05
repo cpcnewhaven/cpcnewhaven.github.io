ef62c67bce812d349357087a05f16555b38e32f2
agBabbbyCakes (ApeWorX, LTD) <alex@apeworx.io>
2024-12-05 13:36:22 -0500

    added basic tab functionality

---
+++ src/js/tab-block.js
@@ -0,0 +1,27 @@
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+
+    // Hide all tab contents
+    tabcontent = document.getElementsByClassName("tab-block-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Remove the active class from all tab links
+    tablinks = document.getElementsByClassName("tab-block-tab");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab and add an active class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Open the first tab by default
+document.addEventListener("DOMContentLoaded", function() {
+    var firstTab = document.querySelector(".tab-block-tab");
+    if (firstTab) {
+        firstTab.click();
+    }
+});
