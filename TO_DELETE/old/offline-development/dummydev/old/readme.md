fa72c99b5795ac6697e56251adcace78324d532f
agbabbbycakes <alex@apeworx.io>
2026-01-21 11:56:00 -0500

    Remove TO_DELETE folder

--- TO_DELETE/old/offline-development/dummydev/old/readme.md
+++
@@ -1,2 +0,0 @@
-
-REAME.md
--- TO_DELETE/old/offline-development/dummydev/old/script/scripts.js
+++
@@ -1,8 +0,0 @@
-function modifiedTitle(pageName = document.title) {
-    lastUpdate = new Date (document.lastModified);
-    updateMonth = (lastUpdate.getMonth() + 1).toString();
-    updateDay = lastUpdate.getDate().toString();
-    updateYear = lastUpdate.getYear().toString().substr(-2);
-
-    document.title = pageName + " | CPC NEW HAVEN | as of " + updateMonth + "." + updateDay + "." + updateYear;
-}
