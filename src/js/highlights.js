0a7f5066fd3c547bc7538dd2e0e8dae47fbc7359
oooAHOYooo <alex@ahoy.ooo>
2026-09-08 09:42:01 -0400

    Update care teams and meeting resources

--- src/js/highlights.js
+++ src/js/highlights.js
@@ -25,7 +25,7 @@ class HighlightsManager {
 
     async loadHighlights() {
         // Add version parameter for cache-busting (update this when highlights.json changes)
-        const cacheBuster = `?v=20260828-2`;
+        const cacheBuster = `?v=20260908-1`;
         const response = await fetch(this.dataUrl + cacheBuster);
 
         if (!response.ok) {
